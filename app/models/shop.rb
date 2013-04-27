77a4322945264be00d76e243df67a056b134617b
Blair Anderson <blair81@gmail.com>
2013-04-27 00:37:58 -0600

    yelp is working

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,17 +5,20 @@ class Shop < ActiveRecord::Base
     :website,
     :wifi,
     :address_attributes
+    # is_claimed, rating, mobile_url, rating_img_url, review_count, snippet_image_url, rating_img_url_small, url, snippet_text, image_url, categories, display_phone, rating_img_url_large, id, is_closed, location
 
   has_one :address
   accepts_nested_attributes_for :address, :allow_destroy => true
 
-  after_validation :yelp=
-
   def to_s
     name.split.join("+")
   end
+  
+  def yelp_hash
+    Yelper.new(@shop).hash
+  end
 
   def yelp
-    @yelp_details ||= Yelper.new(self).result
+    @yelp_details = Yelper.new(@shop).result
   end
 end
--- app/models/yelper.rb
+++ app/models/yelper.rb
@@ -1,12 +1,16 @@
 class Yelper
-  attr_reader :query
+  attr_reader :hash
   def initialize(shop)
     @query = "/v2/search?term=#{shop}&location=Denver&limit=1"
+    @hash ||= JSON.parse(YELPSEARCH.get(@query).body)["businesses"][0]
   end
 
   def result
-    @result ||= JSON.parse(YELPSEARCH.get(@query).body)["businesses"][0]
-    OpenStruct.new(@result)
+    OpenStruct.new(@hash)
+  end
+
+  def hash
+    @hash
   end
 
 end
