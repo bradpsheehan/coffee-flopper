9a81a01fdabde917c5fc09e78fabe36e736b4375
Blair Anderson <blair81@gmail.com>
2013-05-03 13:40:25 -0600

    google map works locally but not production :/

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -6,4 +6,5 @@ token_secret = 'GipsL-8fOnZmGa_jVKXqdM3sq7s'
 api_host = 'api.yelp.com'
 
 consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+
 YELPSEARCH = OAuth::AccessToken.new(consumer, token, token_secret)
